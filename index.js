45a90bfd245110ccff4585cb94b9b58a911098fe
Caleb Reiter <ctr020196@gmail.com>
2020-03-09 14:01:12 -0500

    fuck

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const Discord = require('discord.js');
 const dotenv = require('dotenv');
 const textToOverlay = require('./text-overlay');
+const fs = require('fs');
 
 dotenv.config();
 
-const client = new Discord.client();
+const client = new Discord.Client();
 
 const MOTDS = [
   'I hate Mondays.',
@@ -14,15 +15,19 @@ const MOTDS = [
   'Don\'t forget to do your timecards.'
 ]
 
-const MOTD = MOTDS[(new Date()-1)];
+const MOTD = MOTDS[(new Date().getDay()-1)];
 
 client.on('ready', async () => {
-  const responses = client.guilds.array().map(guild =>
-    guild.systemChannel.send(null, {
-      textToOverlay('./resources/MicrosoftTeams-image.png', MOTD);
-    }));
-  await Promise.all(responses);
+  client.guilds.cache.array().forEach(async guild =>
+    {
+      console.log(MOTD);
+      const files = await textToOverlay('./resources/MicrosoftTeams-image.png', MOTD);
+      await guild.systemChannel.send({files: [files]});
+  });
   client.destroy();
-})
+});
 
-client.login(process.env.CLIENT_TOKEN);
+(async () => {
+  console.log(process.env.CLIENT_TOKEN);
+  await client.login(process.env.CLIENT_TOKEN);
+})()
--- text-overlay.js
+++ text-overlay.js
@@ -1,17 +1,13 @@
 const Jimp = require('jimp')
+const fs = require('fs');
 
 const FONT_PATH = './fonts/SEGOE_24_OUTLINE.fnt'
 
 const textOverlay = async (filepath, text, fontpath=FONT_PATH) => {
-	try {
-        const image = await Jimp.read(filepath);
-        const font = await Jimp.loadFont(fontpath);
-        await image.print(font, 5, 5, {text, alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER}, 464 /* magic number? */);
-        return [null, await image.getBufferAsync(Jimp.AUTO)];
-    } catch (error) {
-    	console.error(error);
-    	return [error];
-    }
+    const image = await Jimp.read(filepath);
+    const font = await Jimp.loadFont(fontpath);
+    image.print(font, 5, 5, {text, alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER}, 464 /* magic number? */);
+    return await image.getBufferAsync(Jimp.AUTO)
 };
 
-export default textOverlay;
+module.exports = textOverlay;
